c1990cc813bab87a52a493b04cc6d7fcfd060d7c
Jhonattan Gomes <www.jhonattan@hotmail.com>
2026-04-05 21:26:31 -0300

    :sparkles:feat adição de upload de pdf para treinamento

--- backend/app/api/v1/chat.py
+++ backend/app/api/v1/chat.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, UploadFile, File
 from app.schemas.chat import ChatRequest, ChatResponse, ContextIngestRequest, ContextIngestResponse
 from app.services.ai_engine import AIService
 from app.services.vector_db import VectorDBService
@@ -22,3 +22,13 @@ async def ingest_context(request: ContextIngestRequest):
     vector_service = VectorDBService()
     total = vector_service.add_context(texts=request.textos, source=request.source)
     return {"adicionados": total, "source": request.source}
+
+@router.post("/upload-pdf")
+async def upload_pdf(file: UploadFile = File(...)):
+    temp_path = f"/tmp/{file.filename}"
+    with open(temp_path, "wb") as buffer:
+        buffer.write(await file.read())
+
+    ai_service = AIService()
+    num_chunks = ai_service.process_pdf(temp_path)
+    return{"message": f"PDF processado com sucesso {num_chunks} trechos adicionados ao conhecimento."}
--- backend/app/services/ai_engine.py
+++ backend/app/services/ai_engine.py
@@ -1,8 +1,9 @@
 from typing import List
-
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import SentenceTransformerEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from app.core.config import settings
 
 
@@ -17,6 +18,17 @@ class AIService:
             model=settings.MODEL_NAME, 
             google_api_key=settings.GOOGLE_API_KEY
         )
+
+    def process_pdf(self, file_path: str):
+        loader = PyPDFLoader(file_path)
+        paginas = loader.load()
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, 
+            chunk_overlap=200,
+            )
+        chunks = text_splitter.split_documents(paginas)
+        self.vectorstore.add_documents(chunks)
+        return len(chunks)
 
     def ask_question(self, question: str, historico: List):
         docs = self.vectorstore.similarity_search(question, k=3)
